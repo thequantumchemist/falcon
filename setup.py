0e7582fa70ab93918558c4b2b6978352da15e3e2
Noah Felis <nfelis@uni-bremen.de>
2025-07-10 13:38:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,29 @@
 from setuptools import setup
 
 setup(
-    name='falcon-md',
-    version='1.0.0',    
-    description='The python distribution for the FALCON on-the-fly Machine Learning ab initio Molceular Dynamcis code',
+    name='falcon_md',
+    version='1.0.0',
+    description='The python distribution for the FALCON on-the-fly Machine Learning ab initio Molceular Dynamics code',
     url='https://github.com/thequantumchemist/falcon',
     author='Noah Felis and  Wilke Dononelli',
     author_email='wido@uni-bremen.de',
     license='GPL-3.0',
-    packages=['falcon','falcon/utils'],
+    packages=['falcon_md','falcon_md/utils', 'falcon_md/models', 'falcon_md/structures', 'falcon_md/utils/analysis'],
+    package_data={
+        'falcon_md/structures': ['*.xyz', '*.traj'],
+    },
+    include_package_data=True,
     install_requires=['agox',
-                      'numpy',  
+                      'numpy',
                       'ase',
-                      'pytest'                   
+                      'pytest'
                       ],
 
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
-        
-        'Operating System :: POSIX :: Linux',       
+
+        'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.8',
     ],
 )
